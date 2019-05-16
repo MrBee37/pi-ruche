b446ab67ddc9891e5a6118c82fd9a580053d50ee
MrBee37 <beeornottobee37@gmail.com>
2019-05-16 18:13:09 +0200

    info lecture vidéo sous anaconda et cv2

--- vidéo/DetectionFrelonSurVideo.py
+++ vidéo/DetectionFrelonSurVideo.py
@@ -9,8 +9,20 @@ Auteur             : lp
 
 '''
 Fonctionne sous linux avec python 2.7 et opencv 2.4.13
-Ne Fonctionne pas sous windows(conda) (open cv limité à 2.4.11)
-info sur la version :
+
+Fonctionne sous windows(anaconda)
+   sur https://www.anaconda.com/distribution/  , prendre didtribution python 2.7
+   la lecture de vidéo via open cv nécessite un accès à ffmpeg 
+   ( non disposnible via installation conda install -c menpo opencv)
+   voir détail sur blog :
+   https://randomstufforganized.wordpress.com/2017/08/16/install-opencv-python-with-ffmpeg-to-anaconda/
+   sur https://opencv.org/releases/ , charger  OpenCV – 2.4.13.6 pour windows
+   copier cv2.pyd de C:\opencv\build\python\2.7\x64 vers  anaconda2\Lib\site-packages
+   ajouter la variable d'environnement : OPENCV_DIR valeur : C:\opencv\build\x64\vc14
+   ajouter le chemin ;%OPENCV_DIR%\bin
+   utiliser spyder pour lancer python  ou une fenêtre Anaconda Prompt
+
+info sur la version opencv:
 from cv2 import __version__
 print "version opencv : ",__version__
 '''
@@ -29,7 +41,7 @@ else:
 while True:
   try:
     ret, img = cap.read()
-    cv2.imshow('Exemple detection',img)
+    #cv2.imshow('Exemple detection',img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     faces = face_cascade.detectMultiScale(gray, 1.3, 12, 0, (90,60), (210,140))
